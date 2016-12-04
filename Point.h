4cab0a1189c9d11a14fa77a784c87f1a7849acbb
Amitai Stern <amitaist@mta.ac.il>
2016-12-04 15:34:13 +0200

    point char

--- Point.h
+++ Point.h
@@ -8,20 +8,24 @@ using namespace std;
 
 class Point {
 	int x, y;
+	char ch;
 public:
-	Point(int _x = 1, int _y = 1) : x(_x), y(_y) {}
-
+	Point(int _x = 1, int _y = 1, char _ch =' ' ) : x(_x), y(_y),ch(_ch) {}
+	char getCH() const {
+		return ch;
+	}
 	int getX() const {
 		return x;
 	}
 	int getY() const {
 		return y;
 	}
-	void set(int _x, int _y) {
+	void set(int _x, int _y, char _ch) {
+		ch = _ch;
 		x = _x;
 		y = _y;
 	}
-	void draw(char ch) {
+	void draw() {
 		gotoxy(x, y);
 		cout << ch;
 		cout.flush();
@@ -29,9 +33,9 @@ public:
 	void move();
 	void move(int direction);
 	Point next(int direction) {//find out what is the next point on the board
-		Point next = *this;//represents the direction
-		next.move(direction);
-		return next;
+		Point p_next = *this;//represents the direction
+		p_next.move(direction);
+		return p_next;
 	}
 };
 
