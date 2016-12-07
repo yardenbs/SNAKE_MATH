61d73f8c5b3480b431be0a9cea7e69195a49cba7
Amitai Stern <amitaist@mta.ac.il>
2016-12-07 13:21:21 +0200

    levels

--- Level.h
+++ Level.h
@@ -30,6 +30,41 @@ public:
 
 };
 
+class DividesBy7 : public Level {
 
+public:
+	DividesBy7() : Level("Eat numbers that divide by 7!"){}
+
+	virtual bool isSol(const int num) { return num % 7 == 0; }
+
+};
+
+class DividesBy4 : public Level {
+
+public:
+	DividesBy4() : Level("Eat numbers that divide by 4!") {}
+
+	virtual bool isSol(const int num) { return num % 4 == 0; }
+
+
+};
+
+class Div7Remain3 : public Level {
+
+public:
+	Div7Remain3() : Level("Eat numbers that have a remainder of 3 when divided by 7!") {}
+
+	virtual bool isSol(const int num) { return num % 7 == 3; }
+
+};
+
+class OneSixtyNine : public Level {
+
+public:
+	OneSixtyNine() : Level("Eat 13 squared!") {}
+
+	virtual bool isSol(const int num) { return num == 169; }
+
+};
 
 #endif // !PLAYGROUND
